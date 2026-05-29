3f5c530da1d852d1bab39530bc7b8b1ca730be11
H.Sato <hiropom77@gmail.com>
2026-05-29 09:40:32 +0900

    Update victory screen text and dialogues according to user request

--- register-sw.js
+++ register-sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js?v=11')
+    navigator.serviceWorker.register('./sw.js?v=12')
       .then((registration) => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hyakumasu-v11';
+const CACHE_NAME = 'hyakumasu-v12';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
