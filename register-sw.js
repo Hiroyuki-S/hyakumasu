b6ad7b339451bc27adef0fb3ed3f61c1de8e1dfe
H.Sato <hiropom77@gmail.com>
2026-05-29 09:37:19 +0900

    Update delete key behavior to clear entire cell answer

--- register-sw.js
+++ register-sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js?v=10')
+    navigator.serviceWorker.register('./sw.js?v=11')
       .then((registration) => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
