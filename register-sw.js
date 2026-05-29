f59535ca8b2d03deef047649552abb5fa91fb79e
H.Sato <hiropom77@gmail.com>
2026-05-29 09:44:00 +0900

    Update victory dialogue trailing emoji to match character fruit

--- register-sw.js
+++ register-sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js?v=12')
+    navigator.serviceWorker.register('./sw.js?v=13')
       .then((registration) => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
